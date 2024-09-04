4b498faf1b7d2489d637ffcc84a94e94dd30a8f7
nalwebdevelopement <naliniu91@gmail.com>
2024-09-04 13:32:28 -0400

    create a first api orders

--- server.js
+++ server.js
@@ -5,6 +5,19 @@ const PORT = 3000
 app.get('/',(req,res)=>{
     res.send("Welcome Home")
 })
+
+
+let orders = [
+        {custid:1,productid: 1001,prodname:'cell phone', Quantity: 1},
+        {custid:2,productid: 1002,prodname:'pencil', Quantity: 10},
+        {custid:3,productid: 91001,prodname:'Dining Table', Quantity: 1},
+    ];
+
+app.get('/api/orders',(req,res)=>
+{
+    res.json(orders);
+});
+
 app.listen(PORT,()=>{
     console.log(`Server is now Running...${PORT}`)
 })
